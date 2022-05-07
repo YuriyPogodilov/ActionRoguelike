3d56e494cb0f1057f6eed0516b47014e8eaa312c
Yuriy Pogodilov <pogodilov.yuri@mytona.com>
2022-05-07 20:03:57 +0800

    Added SBaseProjectile class

---
+++ Source/ActionRoguelike/Private/SBaseProjectile.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SBaseProjectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+
+ASBaseProjectile::ASBaseProjectile()
+{
+	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
+	SphereComp->SetCollisionProfileName("Projectile");
+	RootComponent = SphereComp;
+
+	EffectComp = CreateDefaultSubobject<UParticleSystemComponent>("EffectComp");
+	EffectComp->SetupAttachment(SphereComp);
+
+	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("MovementComp");
+	MovementComp->InitialSpeed = 1000.f;
+	MovementComp->bRotationFollowsVelocity = true;
+	MovementComp->bInitialVelocityInLocalSpace = true;
+}
+
---
+++ Source/ActionRoguelike/Public/SBaseProjectile.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SBaseProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+class UParticleSystemComponent;
+
+
+UCLASS()
+class ACTIONROGUELIKE_API ASBaseProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASBaseProjectile();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USphereComponent* SphereComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UProjectileMovementComponent* MovementComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UParticleSystemComponent* EffectComp;
+};
