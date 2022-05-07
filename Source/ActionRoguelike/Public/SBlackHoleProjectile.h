94cb0032b653f7faa5342358686559836005a3c6
Yuriy Pogodilov <pogodilov.yuri@mytona.com>
2022-05-07 22:42:58 +0800

    Added BlackHoleProjectile

---
+++ Source/ActionRoguelike/Public/SBlackHoleProjectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SBaseProjectile.h"
+#include "SBlackHoleProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API ASBlackHoleProjectile : public ASBaseProjectile
+{
+	GENERATED_BODY()
+	
+};
